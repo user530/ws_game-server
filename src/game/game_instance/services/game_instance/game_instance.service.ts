47de6e736df80f2796809603c47b6c248947ee82
user530 <m.timarsuev@gmail.com>
2023-12-18 19:40:31 +0300

    Fixed gameInstance service - client emits to socket

--- src/game/game_instance/services/game_instance/game_instance.service.ts
+++ src/game/game_instance/services/game_instance/game_instance.service.ts
@@ -2,13 +2,13 @@ import { HttpException, Injectable } from '@nestjs/common';
 import { Socket } from 'socket.io';
 import { MakeTurnDTO, ForfeitMatchDTO } from '../../dtos';
 import { DefaultEventsMap } from 'socket.io/dist/typed-events';
-import { ErrorMessage } from '@user530/ws_game_shared/interfaces';
+import { ErrorMessage, GameCommandDataType } from '@user530/ws_game_shared/interfaces';
 import { createErrorMessage } from '@user530/ws_game_shared/creators';
 import { GameLogicService } from '../game_logic/game_logic.service';
 import { GameStatus } from '@user530/ws_game_shared/enums';
 
 interface IGameInstanceService {
-    handleMakeTurnMessage(client: Socket, payload: MakeTurnDTO): Promise<void>;
+    handleMakeTurnMessage(socket: Socket, client: Socket, payload: MakeTurnDTO): Promise<void>;
     handleForfeitMessage(client: Socket, payload: ForfeitMatchDTO): Promise<void>;
 }
 
@@ -23,6 +23,7 @@ export class GameInstanceService implements IGameInstanceService {
     }
 
     async handleMakeTurnMessage(
+        socket: Socket,
         client: Socket<DefaultEventsMap, DefaultEventsMap, DefaultEventsMap, any>,
         payload: MakeTurnDTO): Promise<void> {
 
@@ -32,25 +33,25 @@ export class GameInstanceService implements IGameInstanceService {
         console.log(turnData);
 
         try {
-            // const validGame = await this.gameLogicService.validateGameTurn(turnData);
             await this.gameLogicService.registerTurn(turnData);
 
             const resultStatus = await this.gameLogicService.processTurn(turnData);         // CHANGE FOR THE PROPER ENUM!
 
+            socket.emit('new_turn', turnData)
+
             if (resultStatus === GameStatus.Completed)
-                client.emit('game_over_win', turnData.player_id);
+                socket.emit('game_over_win', turnData.player_id);
             else if (resultStatus === GameStatus.Aborted)
-                client.emit('game_over_draw');
-            else
-                client.emit('new_turn', turnData)
+                socket.emit('game_over_draw');
 
         } catch (error) {
+            console.log('handleMakeTurnMessage - catch block!');
             if (error instanceof HttpException) {
                 const err = error as HttpException
                 this.emitError(client, createErrorMessage({ code: err.getStatus(), message: err.message }));
             }
             else
-                throw error;
+                this.emitError(client, createErrorMessage({ code: 505, message: error.message }));
         }
     }
 
