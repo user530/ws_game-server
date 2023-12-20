15b5da3437f669fb1e1c75a13b693fabacb9afd1
user530 <m.timarsuev@gmail.com>
2023-12-20 22:29:27 +0300

    Handle make turn message refactored

--- src/game/game_instance/game_instance.module.ts
+++ src/game/game_instance/game_instance.module.ts
@@ -5,9 +5,10 @@ import { DatabaseModule } from 'src/database/database.module';
 import { GameService, GameTurnService } from 'src/database/services';
 import { GameLogicService } from './services/game_logic/game_logic.service';
 import { GameEmitterService } from './services/game_emitter/game_emitter.service';
+import { GameInstanceEventsService } from './services/game_instance_events/game_instance_events.service';
 
 @Module({
   imports: [DatabaseModule],
-  providers: [GameInstanceService, GameInstanceGateway, GameTurnService, GameService, GameLogicService, GameEmitterService]
+  providers: [GameInstanceService, GameInstanceGateway, GameTurnService, GameService, GameLogicService, GameEmitterService, GameInstanceEventsService]
 })
 export class GameInstanceModule { }
--- src/game/game_instance/services/game_instance/game_instance.service.ts
+++ src/game/game_instance/services/game_instance/game_instance.service.ts
@@ -1,90 +1,94 @@
-import { Injectable } from '@nestjs/common';
-import { Socket } from 'socket.io';
+import { HttpException, Injectable } from '@nestjs/common';
 import { MakeTurnDTO, ForfeitMatchDTO } from '../../dtos';
-import { DefaultEventsMap } from 'socket.io/dist/typed-events';
 import { GameLogicService } from '../game_logic/game_logic.service';
-import { GameEventNewTurn, GameEventGameWon, GameEventGameDraw } from '@user530/ws_game_shared/interfaces/ws-events';
+import { GameEventNewTurn, GameEventGameWon, GameEventGameDraw, ErrorEvent } from '@user530/ws_game_shared/interfaces/ws-events';
 import { GameTurnResult } from '@user530/ws_game_shared/enums';
+import { GameInstanceEventsService } from '../game_instance_events/game_instance_events.service';
 
 interface IGameInstanceService {
     handleMakeTurnMessage(payload: MakeTurnDTO):
-        Promise<GameEventNewTurn | [GameEventNewTurn, GameEventGameWon | GameEventGameDraw]>;
+        Promise<GameEventNewTurn | [GameEventNewTurn, GameEventGameWon | GameEventGameDraw] | ErrorEvent>;
     handleForfeitMessage(payload: ForfeitMatchDTO): Promise<GameEventGameWon>;
 }
 
 @Injectable()
 export class GameInstanceService implements IGameInstanceService {
     constructor(
-        private readonly gameLogicService: GameLogicService
+        private readonly gameLogicService: GameLogicService,
+        private readonly eventCreatorService: GameInstanceEventsService,
     ) { }
 
-    async handleMakeTurnMessage(payload: MakeTurnDTO): Promise<GameEventNewTurn | [GameEventNewTurn, GameEventGameWon | GameEventGameDraw]> {
+    async handleMakeTurnMessage(payload: MakeTurnDTO): Promise<
+        GameEventNewTurn
+        | [GameEventNewTurn, GameEventGameWon | GameEventGameDraw]
+        | ErrorEvent
+    > {
         try {
             console.log('Handle MakeTurn fired!');
 
             const { data: turnData } = payload;
             console.log(turnData);
 
+            // Add the turn to the game
             await this.gameLogicService.registerTurn(turnData);
+            console.log('Turn registered');
+            // Update game state depending on the turn
             const newGameState = await this.gameLogicService.processTurn(turnData);
-
+            console.log('Turn processed, new state:', newGameState);
+            // Status of the player turn
             const turnResult = this.gameLogicService.lastTurnResult(
                 {
                     gameStatus: newGameState.status,
                     gameWinner: newGameState.winner
                 }
             );
-
+            console.log('Turn result ', turnResult);
+            // Decide the turn mark based on the turn
             const turnMark = this.gameLogicService.lastTurnMark(
                 {
                     gameHost: newGameState.host,
                     lastTurn: newGameState.turns.slice(-1)[0]
                 }
             );
-
-
-            if (turnResult === GameTurnResult.Not_Decided)
-                return
-            if (turnResult === GameTurnResult.Win)
-                console.log('Game won');
-            else if (turnResult === GameTurnResult.Draw)
-                console.log('Game')
-
-            // Game logic service (turnData) => new Game state
-
-            // Game to Events
-
-            // Emit events
-
-            // await this.gameLogicService.registerTurn(turnData);
-
-            // const resultStatus = await this.gameLogicService.processTurn(turnData);         // CHANGE FOR THE PROPER ENUM!
-
-            // const mark = turnData.player_id === 'bfb9551f-ee05-4b69-b19c-e471f81f3e4d' ? 'X' : 'O'; // DELETE THIS CRAP!
-
-            // CREATE MESSAGES
-
-            // EMITTER SERVICE - EMIT MESSAGES
-
-            // socket.emit('new_turn', { ...turnData, mark })
-
-
-            // if (resultStatus === GameStatus.Completed)
-            //     socket.emit('game_over_win', turnData.player_id);
-            // else if (resultStatus === GameStatus.Aborted)
-            //     socket.emit('game_over_draw');
+            console.log('Turn mark ', turnMark);
+            // New turn event
+            const newTurnEvent = this.eventCreatorService.prepareNewTurnEvent(
+                {
+                    column: turnData.column,
+                    row: turnData.row,
+                    mark: turnMark
+                }
+            );
+            console.log('New turn event ', newTurnEvent);
+            console.log('Before the if block');
+            // Return Event/Events, based on the turn result
+            if (turnResult === GameTurnResult.Win) {
+                const gameWonEvent = this.eventCreatorService.prepareGameWonEvent({ player_id: turnData.player_id });
+                console.log('Game won event ', gameWonEvent);
+                return [newTurnEvent, gameWonEvent];
+            }
+            else if (turnResult === GameTurnResult.Draw) {
+                const gameDrawEvent = this.eventCreatorService.prepareGameDrawEvent();
+                console.log('Game draw event ', gameDrawEvent);
+                return [newTurnEvent, gameDrawEvent];
+            }
+            else {
+                return newTurnEvent;
+            }
 
         } catch (error) {
             console.log('handleMakeTurnMessage - catch block!');
-            // Create err event 
-            // Emit event
-            // if (error instanceof HttpException) {
-            //     const err = error as HttpException
 
-            //     this.emitError(client, createErrorMessage({ code: err.getStatus(), message: err.message }));
-            // }
-            // else
-            //     this.emitError(client, createErrorMessage({ code: 505, message: error.message }));
+            // Default err object
+            const errObject = { status: 500, message: 'Something went wrong' };
+
+            // Reset error data if the error is recognised
+            if (error instanceof HttpException) {
+                errObject.status = error.getStatus();
+                errObject.message = error.message;
+            }
+
+            return this.eventCreatorService.prepareErrorEvent({ code: errObject.status, message: errObject.message });
         }
     }
 
