bba4103ec3d335207fefda34f27617e140f6b14d
user530 <m.timarsuev@gmail.com>
2024-01-10 23:30:18 +0300

    gameHubService handleJoinGameMessage implemented

--- src/game/game_hub/services/game_hub/game_hub.service.ts
+++ src/game/game_hub/services/game_hub/game_hub.service.ts
@@ -2,12 +2,12 @@ import { HttpException, Injectable } from '@nestjs/common';
 import { ErrorEvent, HubEventGameData, HubEventLobbyData, HubEventMovedToLobby, HubEventGamesUpdated, HubEventQuitHub } from '@user530/ws_game_shared/interfaces/ws-events';
 import { HubLogicService } from '../hub_logic/hub_logic.service';
 import { GameHubEventsService } from '../game_hub_events/game_hub_events.service';
-import { HostGameDTO } from '../../dtos';
+import { HostGameDTO, JoinGameDTO } from '../../dtos';
 
 interface IGameHubService {
     handleConnection(): Promise<HubEventGamesUpdated>;
     handleHostGameMessage(payload: HostGameDTO): Promise<ErrorEvent | [HubEventMovedToLobby, HubEventGamesUpdated]>;
-    handleJoinGameMessage(): Promise<string>;
+    handleJoinGameMessage(payload: JoinGameDTO): Promise<ErrorEvent | [HubEventMovedToLobby, HubEventGamesUpdated]>;
     handleLeaveHubMessage(): Promise<HubEventQuitHub>;
 }
 
@@ -30,7 +30,7 @@ export class GameHubService implements IGameHubService {
             const { data } = payload;
 
             // Create game and get lobby data
-            const lobbyData = await this.hubLogicService.getHostedGame(data);
+            const lobbyData = await this.hubLogicService.getHostedLobby(data);
             const movedToLobbyEvent = this.eventCreatorService.prepareMovedToLobbyEvent(lobbyData);
 
             // Get updated lobby list
@@ -52,8 +52,31 @@ export class GameHubService implements IGameHubService {
         }
     }
 
-    async handleJoinGameMessage(): Promise<string> {
-        return 'HUB EVENT - MOVED TO LOBBY TO HOST; HUB EVENT - GAMES UPDATED TO OTHERS';
+    async handleJoinGameMessage(payload: JoinGameDTO): Promise<ErrorEvent | [HubEventMovedToLobby, HubEventGamesUpdated]> {
+        try {
+            const { data } = payload;
+
+            // Join game and get lobby data
+            const lobbyData = await this.hubLogicService.getJoinedLobby(data);
+            const movedToLobbyEvent = this.eventCreatorService.prepareMovedToLobbyEvent(lobbyData);
+
+            // Get updated lobby list
+            const gamesData = await this.hubLogicService.getOpenLobbies();
+            const gamesUpdatedEvent = this.eventCreatorService.prepareGamesUpdatedEvent(gamesData);
+
+            return [movedToLobbyEvent, gamesUpdatedEvent];
+        } catch (error) {
+            // Default err object
+            const errObject = { status: 500, message: 'Something went wrong' };
+
+            // Reset error data if the error is recognised
+            if (error instanceof HttpException) {
+                errObject.status = error.getStatus();
+                errObject.message = error.message;
+            }
+
+            return this.eventCreatorService.prepareErrorEvent({ code: errObject.status, message: errObject.message });
+        }
     }
 
     async handleLeaveHubMessage(): Promise<HubEventQuitHub> {
