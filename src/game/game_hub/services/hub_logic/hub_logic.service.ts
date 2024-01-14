77c41c3fbc161019d68c6b507650bd0d6d5053ad
user530 <m.timarsuev@gmail.com>
2024-01-14 20:24:26 +0300

    Updated version of HubEventGameData and HubEventLobbyData

--- src/game/game_hub/services/hub_logic/hub_logic.service.ts
+++ src/game/game_hub/services/hub_logic/hub_logic.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@nestjs/common';
+import { GameStatus } from '@user530/ws_game_shared/enums';
 import { HubEventGameData, HubEventLobbyData } from '@user530/ws_game_shared/interfaces/ws-events';
 import { HubCommandHostData, HubCommandJoinData } from '@user530/ws_game_shared/interfaces/ws-messages';
 import { Game } from 'src/database/entities';
@@ -46,11 +47,22 @@ export class HubLogicService implements IHubLogicService {
     }
 
     private gameToHubGameData(game: Game): HubEventGameData {
-        const { id: gameId, host: { name: hostName } } = game;
-        return { gameId, hostName };
+        const { id: gameId, host: { id: hostId, name: hostName }, guest, status } = game;
+
+        if (guest !== null)
+            throw new Error('Hub Game - Guest spot already taken!');
+
+        if (status !== GameStatus.Pending)
+            throw new Error('Hub Game - Incorrect status!');
+
+        return { gameId, host: { hostId, hostName }, guest: null, status };
     }
     private gameToHubLobbyData(game: Game): HubEventLobbyData {
-        const { id: gameId, host: { name: hostName } } = game;
-        return { gameId, hostName }
+        const { id: gameId, host: { id: hostId, name: hostName }, guest: { id: guestId, name: guestName }, status } = game;
+
+        if (status !== GameStatus.Pending)
+            throw new Error('Hub Game - Incorrect status!');
+
+        return { gameId, host: { hostId, hostName }, guest: { guestId, guestName }, status }
     }
 }
