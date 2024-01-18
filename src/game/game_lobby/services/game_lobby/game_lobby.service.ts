064b071df113cdf9a9515798f12e4a136b320b2a
user530 <m.timarsuev@gmail.com>
2024-01-18 23:49:50 +0300

    Game Lobby - handleConnection implemented

--- src/game/game_lobby/services/game_lobby/game_lobby.service.ts
+++ src/game/game_lobby/services/game_lobby/game_lobby.service.ts
@@ -1,9 +1,12 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, Injectable } from '@nestjs/common';
 import { LobbyEventGuestJoined, LobbyEventGuestLeft, LobbyEventMovedToGame, LobbyEventMovedToHub, ErrorEvent } from '@user530/ws_game_shared/interfaces/ws-events';
 import { GameLobbyEventsService } from '../game_lobby_events/game_lobby_events.service';
+import { LobbyLogicService } from '../lobby_logic/lobby_logic.service';
+import { LobbyAuthData } from '../../dtos/lobby_auth.dto';
+import { createLobbyGuestJoinedEvent } from '@user530/ws_game_shared/creators/events';
 
 interface IGameLobbyService {
-    handleConnection(): Promise<void>;
+    handleConnection(authData: LobbyAuthData): Promise<null | LobbyEventGuestJoined | ErrorEvent>;
     handleLeaveLobbyMessage(): Promise<void>;
     handleKickGuestMessage(): Promise<void>;
     handleStartGameMessage(): Promise<void>;
@@ -14,11 +17,34 @@ export class GameLobbyService implements IGameLobbyService {
 
     constructor(
         private readonly eventCreatorService: GameLobbyEventsService,
+        private readonly lobbyLogicService: LobbyLogicService,
     ) { }
 
-    async handleConnection(): Promise<void> {
+    async handleConnection(authData: LobbyAuthData): Promise<null | LobbyEventGuestJoined | ErrorEvent> {
         console.log('Game Lobby Service - Handle Connection Fired');
-        return
+        try {
+            // Validate and get lobby data
+            const gameData = await this.lobbyLogicService.validateLobbyConnection(authData);
+            console.log('Handle connection, validated game data');
+            console.log(gameData);
+            // If connected player is a guest, prepare event
+            if (gameData.guest && gameData.guest.guestId === authData.userId)
+                return createLobbyGuestJoinedEvent({ ...gameData.guest })
+
+            // Host connected, return null
+            else return null
+        } catch (error) {
+            // Default err object
+            const errObject = { status: 500, message: 'Something went wrong' };
+
+            // Reset error data if the error is recognised
+            if (error instanceof HttpException) {
+                errObject.status = error.getStatus();
+                errObject.message = error.message;
+            }
+
+            return this.eventCreatorService.prepareErrorEvent({ code: errObject.status, message: errObject.message });
+        }
     }
 
     async handleLeaveLobbyMessage(): Promise<void> {
