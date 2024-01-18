ab2c02c8430d51164abbd673195cbd2fe683ae7a
user530 <m.timarsuev@gmail.com>
2024-01-18 23:50:16 +0300

    Lobby gateway - initial implementation

--- src/game/game_lobby/game_lobby.module.ts
+++ src/game/game_lobby/game_lobby.module.ts
@@ -3,14 +3,19 @@ import { GameLobbyGateway } from './gateways/game_lobby/game_lobby.gateway';
 import { GameLobbyService } from './services/game_lobby/game_lobby.service';
 import { GameLobbyEventsService } from './services/game_lobby_events/game_lobby_events.service';
 import { LobbyLogicService } from './services/lobby_logic/lobby_logic.service';
+import { GameService, PlayerService } from 'src/database/services';
+import { DatabaseModule } from 'src/database/database.module';
+
 
 @Module({
-    imports: [],
+    imports: [DatabaseModule],
     providers: [
         GameLobbyGateway,
         GameLobbyService,
         GameLobbyEventsService,
         LobbyLogicService,
+        GameService,
+        PlayerService,
     ],
 })
 export class GameLobbyModule { }
--- src/game/game_lobby/gateways/game_lobby/game_lobby.gateway.ts
+++ src/game/game_lobby/gateways/game_lobby/game_lobby.gateway.ts
@@ -1,47 +1,80 @@
-import { SubscribeMessage, WebSocketGateway, OnGatewayConnection, OnGatewayDisconnect } from '@nestjs/websockets';
+import { SubscribeMessage, WebSocketGateway, OnGatewayConnection, OnGatewayDisconnect, WebSocketServer } from '@nestjs/websockets';
 import { GameLobbyMessagesHandler } from '@user530/ws_game_shared/interfaces/ws-listeners';
 import { LobbyCommandKickGuest, LobbyCommandLeaveLobby, LobbyCommandStartGame } from '@user530/ws_game_shared/interfaces/ws-messages';
-import { LobbyCommand } from '@user530/ws_game_shared/types';
+import { LobbyCommand, MessageType } from '@user530/ws_game_shared/types';
 import { GameLobbyService } from '../../services/game_lobby/game_lobby.service';
+import { Socket, Server } from 'socket.io';
+import { UseGuards } from '@nestjs/common';
 
 @WebSocketGateway({
   cors: '*',
   namespace: '/lobby',
 })
 export class GameLobbyGateway implements GameLobbyMessagesHandler, OnGatewayConnection, OnGatewayDisconnect {
+  @WebSocketServer()
+  private server: Server;
 
   constructor(
     private readonly gameLobbyService: GameLobbyService
   ) { }
 
-  async handleConnection(client: any) {
+  async handleConnection(client: Socket) {
     console.log('LOBBY GATEWAY - CONNECTION ESTABLISHED');
-    const someEvents = await this.gameLobbyService.handleConnection();
-    return
+    console.log(client.handshake.auth);
+    const { gameId, userId } = client.handshake.auth;
+    // Update Lobby Status event to client, if guest joined also send Update Lobby Status to the host;
+    const connectionEvent = await this.gameLobbyService.handleConnection({ gameId, userId });
+    console.log('CONNECTION EVENT: ');
+    console.log(connectionEvent)
+    // If not restored -> Join lobby room
+    if (!client.recovered) {
+      console.log('Connecting user to the room');
+      client.join(gameId);
+    }
+
+    // No event needed
+    if (connectionEvent === null) return;
+
+    // Inform host about the updated game
+    if (connectionEvent.type === MessageType.LobbyEvent) {
+      console.log('Broadcast to the host Guest Joined event');
+      client.broadcast.to(gameId).emit(connectionEvent.command, connectionEvent)
+    }
+    // Emit Error and disconnect user 
+    else {
+      console.log('Error event emit')
+      client.emit(connectionEvent.type, connectionEvent);
+      client.disconnect();
+    }
   }
 
-  handleDisconnect(client: any) {
+  handleDisconnect(client: Socket) {
     console.log('LOBBY GATEWAY - CONNECTION CLOSED');
     return
   }
 
   @SubscribeMessage(LobbyCommand.LeaveLobby)
-  async wsLobbyLeaveLobbyListener(socket: unknown, leaveLobbyMessage: LobbyCommandLeaveLobby): Promise<void> {
+  async wsLobbyLeaveLobbyListener(socket: Socket, leaveLobbyMessage: LobbyCommandLeaveLobby): Promise<void> {
     console.log('LOBBY GATEWAY - LEAVE LOBBY MESSAGE RECIEVED');
+    console.log(socket.data)
+    // If user is host -> Abort the game, emit to both: Move to Hub event
+    // If user is guest -> Update the game (guest is null), Update New Lobby Status to host, Emit to guest Move to Hub, Emit updated game list to Hub(!) 
     const someEvents = await this.gameLobbyService.handleLeaveLobbyMessage();
     return
   }
 
   @SubscribeMessage(LobbyCommand.KickGuest)
-  async wsLobbyKickGuestListener(socket: unknown, kickGuestMessage: LobbyCommandKickGuest): Promise<void> {
+  async wsLobbyKickGuestListener(socket: Socket, kickGuestMessage: LobbyCommandKickGuest): Promise<void> {
     console.log('LOBBY GATEWAY - KICK GUEST MESSAGE RECIEVED');
+    // Check that player is host, If true -> Update the game (guest is null), Update lobby for the host, Emit to guest Move to Hub, Emit updated game list to Hub(!)
     const someEvents = await this.gameLobbyService.handleKickGuestMessage();
     return
   }
 
   @SubscribeMessage(LobbyCommand.StartGame)
-  async wsLobbyStartGameListener(socket: unknown, startGameMessage: LobbyCommandStartGame): Promise<void> {
+  async wsLobbyStartGameListener(socket: Socket, startGameMessage: LobbyCommandStartGame): Promise<void> {
     console.log('LOBBY GATEWAY - START GAME MESSAGE RECIEVED');
+    // Check that player is host, if true -> Update the game (status -> in progress), Emit Move to Game to the game room
     const someEvents = await this.gameLobbyService.handleStartGameMessage();
     return
   }
