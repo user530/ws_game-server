b24f114820bbb78effcb22993603d1cb2a997f8a
user530 <m.timarsuev@gmail.com>
2024-01-19 23:51:25 +0300

    GameHub service - handleConnection updated to handle "restoration", work in progress

--- src/game/game_hub/services/game_hub/game_hub.service.ts
+++ src/game/game_hub/services/game_hub/game_hub.service.ts
@@ -2,10 +2,11 @@ import { HttpException, Injectable } from '@nestjs/common';
 import { ErrorEvent, HubEventMovedToLobby, HubEventGamesUpdated, HubEventQuitHub } from '@user530/ws_game_shared/interfaces/ws-events';
 import { HubLogicService } from '../hub_logic/hub_logic.service';
 import { GameHubEventsService } from '../game_hub_events/game_hub_events.service';
-import { HostGameDTO, JoinGameDTO } from '../../dtos';
+import { HostGameDTO, HubAuthDTO, JoinGameDTO } from '../../dtos';
+import { GameStatus } from '@user530/ws_game_shared/enums';
 
 interface IGameHubService {
-    handleConnection(): Promise<HubEventGamesUpdated>;
+    handleConnection(hubAuthDTO: HubAuthDTO): Promise<ErrorEvent | HubEventMovedToLobby | HubEventGamesUpdated>;
     handleHostGameMessage(payload: HostGameDTO): Promise<ErrorEvent | [HubEventMovedToLobby, HubEventGamesUpdated]>;
     handleJoinGameMessage(payload: JoinGameDTO): Promise<ErrorEvent | [HubEventMovedToLobby, HubEventGamesUpdated]>;
     handleLeaveHubMessage(): Promise<HubEventQuitHub>;
@@ -18,11 +19,39 @@ export class GameHubService implements IGameHubService {
         private readonly eventCreatorService: GameHubEventsService
     ) { }
 
-    async handleConnection(): Promise<HubEventGamesUpdated> {
+    async handleConnection(hubAuthDTO: HubAuthDTO): Promise<ErrorEvent | HubEventMovedToLobby | HubEventGamesUpdated> {
+        try {
+            // Validate user credentials
+            await this.hubLogicService.isValidHubConnection(hubAuthDTO);
 
-        const gamesData = await this.hubLogicService.getOpenLobbies();
+            // Check if user is already has an open game 
+            const openGame = await this.hubLogicService.getPlayerActiveGame(hubAuthDTO);
 
-        return this.eventCreatorService.prepareGamesUpdatedEvent(gamesData);
+            // Prepare game DTO and event accordingly
+            if (openGame && openGame.status === GameStatus.Pending) {
+
+            }
+
+            if (openGame && openGame.status === GameStatus.InProgress) {
+                // PLACEHOLDER -> CREATE NEW HUB EVENT: MOVE TO GAME and prepare data for it here!!!
+            }
+
+            // If there is no pending or active games -> prepare lobby list for the player
+            const gamesData = await this.hubLogicService.getOpenLobbies();
+
+            return this.eventCreatorService.prepareGamesUpdatedEvent(gamesData);
+        } catch (error) {
+            // Default err object
+            const errObject = { status: 500, message: 'Something went wrong' };
+
+            // Reset error data if the error is recognised
+            if (error instanceof HttpException) {
+                errObject.status = error.getStatus();
+                errObject.message = error.message;
+            }
+
+            return this.eventCreatorService.prepareErrorEvent({ code: errObject.status, message: errObject.message });
+        }
     }
 
     async handleHostGameMessage(payload: HostGameDTO): Promise<ErrorEvent | [HubEventMovedToLobby, HubEventGamesUpdated]> {
