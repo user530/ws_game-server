0d8f8e1d8ade7b85342e21e8cbfab928f4f06419
user530 <m.timarsuev@gmail.com>
2024-01-23 23:21:26 +0300

    Lobby service - implementd HandleLeaveLobbyMessage (not tested)

--- src/game/game_lobby/services/game_lobby/game_lobby.service.ts
+++ src/game/game_lobby/services/game_lobby/game_lobby.service.ts
@@ -2,13 +2,14 @@ import { HttpException, Injectable } from '@nestjs/common';
 import { LobbyEventGuestJoined, LobbyEventGuestLeft, LobbyEventMovedToGame, LobbyEventMovedToHub, ErrorEvent } from '@user530/ws_game_shared/interfaces/ws-events';
 import { GameLobbyEventsService } from '../game_lobby_events/game_lobby_events.service';
 import { LobbyLogicService } from '../lobby_logic/lobby_logic.service';
-import { LobbyAuthDTO } from '../../dtos';
-import { createLobbyGuestJoinedEvent, createLobbyToGameEvent } from '@user530/ws_game_shared/creators/events';
+import { LeaveLobbyDTO, LobbyAuthDTO } from '../../dtos';
+import { createLobbyGuestJoinedEvent, createLobbyGuestLeftEvent, createLobbyToGameEvent, createLobbyToHubEvent } from '@user530/ws_game_shared/creators/events';
 import { GameStatus } from '@user530/ws_game_shared/enums';
+import { LeaveLobbyDataType } from '../../dtos/leave_lobby.dto';
 
 interface IGameLobbyService {
     handleConnection(authData: LobbyAuthDTO): Promise<null | LobbyEventMovedToGame | LobbyEventGuestJoined | [ErrorEvent, LobbyEventMovedToHub]>;
-    handleLeaveLobbyMessage(): Promise<void>;
+    handleLeaveLobbyMessage(leaveData: LeaveLobbyDataType): Promise<LobbyEventMovedToHub | [LobbyEventMovedToHub, LobbyEventGuestLeft] | ErrorEvent>;
     handleKickGuestMessage(): Promise<void>;
     handleStartGameMessage(): Promise<void>;
 }
@@ -57,9 +58,39 @@ export class GameLobbyService implements IGameLobbyService {
         }
     }
 
-    async handleLeaveLobbyMessage(): Promise<void> {
-        console.log('Game Lobby Service - Handle Leave Lobby Message Fired');
-        return
+    async handleLeaveLobbyMessage(leaveData: LeaveLobbyDataType): Promise<LobbyEventMovedToHub | [LobbyEventMovedToHub, LobbyEventGuestLeft] | ErrorEvent> {
+        try {
+            console.log('Game Lobby Service - Handle Leave Lobby Message Fired');
+            const { gameId, playerId } = leaveData;
+            console.log(`GameId: ${gameId}, playerId: ${playerId}`);
+            const isHost = await this.lobbyLogicService.isPlayerHost(leaveData);
+            console.log('Handle leave message, is host - ', isHost);
+            // If user is host -> Abort the game, emit to both: Move to Hub event
+            if (isHost) {
+                await this.lobbyLogicService.handleHostLeave(gameId);
+                return createLobbyToHubEvent();
+            }
+            // If user is guest -> Update the game (guest is null), Update New Lobby Status to host, Emit to guest Move to Hub, Emit updated game list to Hub(!) 
+            else {
+                await this.lobbyLogicService.handleGuestLeave(gameId);
+                // NEED SOME WAY TO EMIT UPDATED GAME LIST IN THE HUB! (Maybe add some update msg listener to the hub and emit from the lobby gateway to the hub one)
+                return [
+                    createLobbyToHubEvent(),
+                    createLobbyGuestLeftEvent(),
+                ]
+            }
+        } catch (error) {
+            // Default err object
+            const errObject = { status: 500, message: 'Something went wrong' };
+
+            // Reset error data if the error is recognised
+            if (error instanceof HttpException) {
+                errObject.status = error.getStatus();
+                errObject.message = error.message;
+            }
+
+            return this.eventCreatorService.prepareErrorEvent({ code: errObject.status, message: errObject.message });
+        }
     }
 
     async handleKickGuestMessage(): Promise<void> {
