7f1162df2eae355130e47b0535ab3a1e8aa3bdb2
user530 <m.timarsuev@gmail.com>
2024-01-23 20:05:14 +0300

    Leave lobby DTO implemented

--- src/game/game_lobby/dtos/index.ts
+++ src/game/game_lobby/dtos/index.ts
@@ -1,7 +1,9 @@
 import { LobbyAuthDTO } from './lobby_auth.dto';
 import { LobbyDataDTO } from './lobby_data.dto';
+import { LeaveLobbyDTO } from './leave_lobby.dto';
 
 export {
     LobbyAuthDTO,
     LobbyDataDTO,
+    LeaveLobbyDTO,
 }
---
+++ src/game/game_lobby/dtos/leave_lobby.dto.ts
@@ -0,0 +1,38 @@
+import { LobbyCommandLeaveLobby, LobbyCommandLeaveData } from '@user530/ws_game_shared/interfaces/ws-messages';
+import { LobbyCommand, MessageType } from '@user530/ws_game_shared/types';
+import { Type } from 'class-transformer';
+import { Equals, IsEnum, IsNotEmpty, IsNumber, IsObject, IsString, IsUUID, ValidateNested } from 'class-validator';
+
+class LeaveLobbyDataType implements LobbyCommandLeaveData {
+    @IsNotEmpty()
+    @IsString()
+    @IsUUID()
+    gameId: string;
+
+    @IsNotEmpty()
+    @IsString()
+    @IsUUID()
+    playerId: string;
+}
+
+export class LeaveLobbyDTO implements LobbyCommandLeaveLobby {
+    @IsNotEmpty()
+    @IsNumber()
+    @Equals(1)
+    version: 1;
+
+    @IsNotEmpty()
+    @IsEnum(MessageType)
+    @Equals(MessageType.LobbyCommand)
+    type: MessageType.LobbyCommand;
+
+    @IsNotEmpty()
+    @IsEnum(LobbyCommand)
+    @Equals(LobbyCommand.LeaveLobby)
+    command: LobbyCommand.LeaveLobby;
+
+    @IsObject()
+    @ValidateNested()
+    @Type(() => LeaveLobbyDataType)
+    data: LeaveLobbyDataType;
+}
