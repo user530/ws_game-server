88323447b29f185b28571a09503c868d6053fb19
user530 <m.timarsuev@gmail.com>
2024-01-24 21:31:21 +0300

    Lobby StartGameDTO implemented

--- src/game/game_lobby/dtos/index.ts
+++ src/game/game_lobby/dtos/index.ts
@@ -2,6 +2,7 @@ import { LobbyAuthDTO } from './lobby_auth.dto';
 import { LobbyDataDTO } from './lobby_data.dto';
 import { LeaveLobbyDTO, LeaveLobbyDataType } from './leave_lobby.dto';
 import { KickGuestDTO, KickGuestDataType } from './kick_guest.dto';
+import { StartGameDTO, StartGameDataType } from './start_game.dto';
 
 export {
     LobbyAuthDTO,
@@ -10,4 +11,6 @@ export {
     LeaveLobbyDataType,
     KickGuestDTO,
     KickGuestDataType,
+    StartGameDTO,
+    StartGameDataType,
 }
