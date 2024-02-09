a52064186ad29959fde55eacc9795b776221433f
user530 <m.timarsuev@gmail.com>
2024-02-09 23:55:21 +0300

    Game chat service - updated version

--- src/game/game_chat/services/game_chat/game_chat.service.ts
+++ src/game/game_chat/services/game_chat/game_chat.service.ts
@@ -2,14 +2,19 @@ import { Injectable } from '@nestjs/common';
 import { ChatLogicService } from '../chat_logic/chat_logic.service';
 import { GameChatEventsService } from '../game_chat_events/game_chat_events.service';
 import { ChatEventNewMessage, ChatEventNewMsgData } from '@user530/ws_game_shared/interfaces/ws-events';
-import { GeneralMessage } from 'src/database/entities';
-import { ChatCommandSendMsgData } from '@user530/ws_game_shared/interfaces/ws-messages';
+import { LayerMessage, TargetedEvent } from '../../interfaces';
 
 interface IGameChatService {
-    handleHubConnection(): Promise<ChatEventNewMessage[]>;
-    handleLobbyConnection(lobbyRoomId: string): Promise<ChatEventNewMessage[]>;
-    handleGameConnection(gameRoomId: string): Promise<ChatEventNewMessage[]>;
-    handleSendMsgMessage(payload: any): Promise<ErrorEvent | ChatEventNewMessage>;
+    handleHubConnection(connectionData: { userId: string })
+        : Promise<ChatEventNewMessage[] | TargetedEvent>;
+
+    handleLobbyConnection(connectionData: { userId: string, lobbyRoomId: string })
+        : Promise<ChatEventNewMessage[] | TargetedEvent>;
+
+    handleGameConnection(connectionData: { userId: string, gameRoomId: string })
+        : Promise<ChatEventNewMessage[] | TargetedEvent>;
+
+    handleSendMsgMessage(payload: LayerMessage): Promise<ChatEventNewMessage | TargetedEvent>;
 }
 
 @Injectable()
@@ -20,57 +25,189 @@ export class GameChatService implements IGameChatService {
         private readonly eventCreatorService: GameChatEventsService,
     ) { }
 
-    async handleHubConnection(): Promise<ChatEventNewMessage[]> {
+    async handleHubConnection(connectionData: { userId: string })
+        : Promise<ChatEventNewMessage[] | TargetedEvent> {
         try {
             console.log('GAME CHAT SERVICE - HANDLE HUB CONNECTION FIRED!');
 
-            const messages = await this.chatLogicService.getHubMessages();
+            const hubMessages = await this.chatLogicService.getHubMessages();
+            const directMessages = await this.chatLogicService.getDirectMessages(connectionData.userId);
 
-            return messages
-                .map(
-                    msg => this.eventCreatorService
-                        .prepareNewMessageEvent(
-                            this.generalMsgToEventData(msg)
-                        ));
+            const hubEventsData = hubMessages.map(msg => this.chatLogicService.generalMsgToEventData(msg));
+            const directEventsData = directMessages.map(msg => this.chatLogicService.directMsgToEventData(msg))
+
+            // Combine layer messages, direct messages and sort them all 
+            const allUserMsgs = hubEventsData.concat(directEventsData).sort(
+                (msg1, msg2) => {
+                    const time1 = this.chatLogicService.chatstampToMins(msg1.timestamp);
+                    const time2 = this.chatLogicService.chatstampToMins(msg2.timestamp);
+
+                    return (time1 - time2);
+                }
+            )
+
+            const allMessagesEvents = allUserMsgs.map(
+                eventData => this.eventCreatorService.prepareNewMessageEvent(eventData));
+
+            return allMessagesEvents;
 
         } catch (error) {
             console.log('CHAT SERVICE - HANDLE HUB ERROR!');
             console.log(error);
+
+            const errText = error.message || 'SOMETHING WENT WRONG. PLEASE TRY AGAIN LATER!';
+            const eventData: ChatEventNewMsgData = this.chatLogicService.errTxtToEventData(errText);
+            const targetedEvent: TargetedEvent = {
+                event: this.eventCreatorService.prepareNewMessageEvent(eventData),
+                targets: [connectionData.userId],
+            }
+
+            return targetedEvent;
         }
     }
 
-    async handleLobbyConnection(lobbyRoomId: string): Promise<ChatEventNewMessage[]> {
+    async handleLobbyConnection(connectionData: { userId: string, lobbyRoomId: string })
+        : Promise<ChatEventNewMessage[] | TargetedEvent> {
         console.log('GAME CHAT SERVICE - HANDLE LOBBY CONNECTION FIRED!');
-        return [];
+        try {
+            const { lobbyRoomId } = connectionData;
+            const lobbyMessages = await this.chatLogicService.getLobbyMessages(lobbyRoomId);
+            const directMessages = await this.chatLogicService.getDirectMessages(connectionData.userId);
+
+            const lobbyEventsData = lobbyMessages.map(msg => this.chatLogicService.generalMsgToEventData(msg));
+            const directEventsData = directMessages.map(msg => this.chatLogicService.directMsgToEventData(msg))
+
+            // Combine layer messages, direct messages and sort them all 
+            const allUserMsgs = lobbyEventsData.concat(directEventsData).sort(
+                (msg1, msg2) => {
+                    const time1 = this.chatLogicService.chatstampToMins(msg1.timestamp);
+                    const time2 = this.chatLogicService.chatstampToMins(msg2.timestamp);
+
+                    return (time1 - time2);
+                }
+            )
+
+            const allMessagesEvents = allUserMsgs.map(
+                eventData => this.eventCreatorService.prepareNewMessageEvent(eventData));
+
+            return allMessagesEvents;
+
+        } catch (error) {
+            console.log('CHAT SERVICE - HANDLE LOBBY ERROR!');
+            console.log(error);
+
+            const errText = error.message || 'SOMETHING WENT WRONG. PLEASE TRY AGAIN LATER!';
+            const eventData: ChatEventNewMsgData = this.chatLogicService.errTxtToEventData(errText);
+            const targetedEvent: TargetedEvent = {
+                event: this.eventCreatorService.prepareNewMessageEvent(eventData),
+                targets: [connectionData.userId],
+            }
+
+            return targetedEvent;
+        }
     }
 
-    async handleGameConnection(gameRoomId: string): Promise<ChatEventNewMessage[]> {
+    async handleGameConnection(connectionData: { userId: string, gameRoomId: string })
+        : Promise<ChatEventNewMessage[] | TargetedEvent> {
         console.log('GAME CHAT SERVICE - HANDLE GAME CONNECTION FIRED!');
-        return [];
+        try {
+            const { gameRoomId } = connectionData;
+            const gameMessages = await this.chatLogicService.getGameMessages(gameRoomId);
+            const directMessages = await this.chatLogicService.getDirectMessages(connectionData.userId);
+
+            const gameEventsData = gameMessages.map(msg => this.chatLogicService.generalMsgToEventData(msg));
+            const directEventsData = directMessages.map(msg => this.chatLogicService.directMsgToEventData(msg))
+
+            // Combine layer messages, direct messages and sort them all 
+            const allUserMsgs = gameEventsData.concat(directEventsData).sort(
+                (msg1, msg2) => {
+                    const time1 = this.chatLogicService.chatstampToMins(msg1.timestamp);
+                    const time2 = this.chatLogicService.chatstampToMins(msg2.timestamp);
+
+                    return (time1 - time2);
+                }
+            )
+
+            const allMessagesEvents = allUserMsgs.map(
+                eventData => this.eventCreatorService.prepareNewMessageEvent(eventData));
+
+            return allMessagesEvents;
+
+        } catch (error) {
+            console.log('CHAT SERVICE - HANDLE GAME ERROR!');
+            console.log(error);
+
+            const errText = error.message || 'SOMETHING WENT WRONG. PLEASE TRY AGAIN LATER!';
+            const eventData: ChatEventNewMsgData = this.chatLogicService.errTxtToEventData(errText);
+            const targetedEvent: TargetedEvent = {
+                event: this.eventCreatorService.prepareNewMessageEvent(eventData),
+                targets: [connectionData.userId],
+            }
+
+            return targetedEvent;
+        }
     }
 
-    async handleSendMsgMessage(payload: ChatCommandSendMsgData): Promise<ErrorEvent | ChatEventNewMessage> {
-        console.log('GAME CHAT SERVICE - HANDLE SEND MSG MESSAGE FIRED!');
-        const { user, message } = payload;
+    async handleSendMsgMessage(payload: LayerMessage): Promise<ChatEventNewMessage | TargetedEvent> {
+        try {
+            console.log('GAME CHAT SERVICE - HANDLE SEND MSG MESSAGE FIRED!');
+            const { msgData: { message, user }, contextData: { layer, roomId } } = payload;
 
-        // Check if message is DM or general one
-        // If it is DM -> getTarget from the message text
-        // Try to add DM
-        // Return DM event
+            let resEvent: ChatEventNewMessage | TargetedEvent;
 
-        // If general message
-        // this.chatLogicService
+            const isDm = this.chatLogicService.isDirectMessage(message);
 
-        return
+            if (isDm) {
+                // Parse a direct message
+                const { target, messageTxt } = this.chatLogicService.parseDirectMessage(message);
+
+                // Create new direct message from the parsed message
+                const newDm = await this.chatLogicService.addDirectMessage(
+                    { authorId: user, message: messageTxt, targetName: target });
+
+                // Prepare event data
+                const eventData = this.chatLogicService.directMsgToEventData(newDm);
+
+                // Targeted result event
+                resEvent = {
+                    event: this.eventCreatorService.prepareNewMessageEvent(eventData),
+                    targets: [newDm.author.id, newDm.target.id],
+                } as TargetedEvent;
+
+            }
+            else {
+                // Create new general message tied to the layer and the room
+                const newGeneralMessage = await this.chatLogicService.addGeneralMessage(
+                    {
+                        authorId: user,
+                        message,
+                        layer,
+                        roomId,
+                    }
+                )
+
+                // Prepare event data
+                const eventData = this.chatLogicService.generalMsgToEventData(newGeneralMessage);
+
+                // Prepare normal message event
+                resEvent = this.eventCreatorService.prepareNewMessageEvent(eventData);
+            }
+
+            return resEvent;
+
+        } catch (error) {
+            console.log('CHAT SERVICE - HANDLE GAME ERROR!');
+            console.log(error);
+
+            const errText = error.message || 'SOMETHING WENT WRONG. PLEASE TRY AGAIN LATER!';
+            const eventData: ChatEventNewMsgData = this.chatLogicService.errTxtToEventData(errText);
+            const targetedEvent: TargetedEvent = {
+                event: this.eventCreatorService.prepareNewMessageEvent(eventData),
+                targets: [payload.msgData.user],
+            }
+
+            return targetedEvent;
+        }
     }
 
-    private generalMsgToEventData(generalMsg: GeneralMessage): ChatEventNewMsgData {
-        const { author: { name }, message, timestamp } = generalMsg;
-        return {
-            user: name,
-            message,
-            timestamp: [timestamp.getHours(), timestamp.getMinutes()],
-            isWhisper: false
-        };
-    }
 }
