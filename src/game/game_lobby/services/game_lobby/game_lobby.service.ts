d6d3ad825010ce3f07930bb933d8800b66cb6484
user530 <m.timarsuev@gmail.com>
2024-02-11 20:56:34 +0300

    Lobby services updated - clean up

--- src/game/game_lobby/services/game_lobby/game_lobby.service.ts
+++ src/game/game_lobby/services/game_lobby/game_lobby.service.ts
@@ -24,13 +24,9 @@ export class GameLobbyService implements IGameLobbyService {
     ) { }
 
     async handleConnection(authData: LobbyAuthDTO): Promise<null | LobbyEventMovedToGame | LobbyEventGuestJoined | [ErrorEvent, LobbyEventMovedToHub]> {
-        console.log('Game Lobby Service - Handle Connection Fired');
         try {
             // Validate and get lobby data
             const { gameId, guest, host, status, turns } = await this.lobbyLogicService.isValidLobbyConnection(authData);
-
-            console.log('Handle connection, validated game data');
-            console.log({ gameId, guest, host, status, turns });
 
             // If player is already part of the active game
             if (status === GameStatus.InProgress)
@@ -61,16 +57,17 @@ export class GameLobbyService implements IGameLobbyService {
 
     async handleLeaveLobbyMessage(leaveData: LeaveLobbyDataType): Promise<[LobbyEventMovedToHub, HubEventGamesUpdated] | [LobbyEventMovedToHub, LobbyEventGuestLeft, HubEventGamesUpdated] | ErrorEvent> {
         try {
-            console.log('Game Lobby Service - Handle Leave Lobby Message Fired');
-            const { gameId, playerId } = leaveData;
-            console.log(`GameId: ${gameId}, playerId: ${playerId}`);
+            const { gameId } = leaveData;
+
             const isHost = await this.lobbyLogicService.isPlayerHost(leaveData);
-            console.log('Handle leave message, is host - ', isHost);
+
             // If user is host -> Abort the game, emit to both: Move to Hub event
             if (isHost) {
                 await this.lobbyLogicService.handleHostLeave(gameId);
+
                 // Prepare update event for the users in the hub, signalling that game is not available any more
                 const lobbiesData = await this.hubLogicService.getOpenLobbies();
+
                 return [
                     createLobbyToHubEvent(),
                     createHubGamesUpdatedEvent(lobbiesData),
@@ -79,8 +76,10 @@ export class GameLobbyService implements IGameLobbyService {
             // If user is guest -> Update the game (guest is null), Update New Lobby Status to host, Emit to guest Move to Hub, Emit updated game list to Hub(!) 
             else {
                 await this.lobbyLogicService.handleGuestLeave(gameId);
+
                 // Prepare update event for the users in the hub, signalling that game is open again
                 const lobbiesData = await this.hubLogicService.getOpenLobbies();
+
                 return [
                     createLobbyToHubEvent(),
                     createLobbyGuestLeftEvent(),
@@ -103,19 +102,19 @@ export class GameLobbyService implements IGameLobbyService {
 
     async handleKickGuestMessage(kickData: KickGuestDataType): Promise<[LobbyEventMovedToHub, LobbyEventGuestLeft, HubEventGamesUpdated] | ErrorEvent> {
         try {
-            console.log('Game Lobby Service - Handle Kick Guest Message Fired');
-            const { gameId, playerId } = kickData;
-            console.log(`GameId: ${gameId}, playerId: ${playerId}`);
+            const { gameId } = kickData;
+
             const isHost = await this.lobbyLogicService.isPlayerHost(kickData);
-            console.log('Handle kick message, is host - ', isHost);
 
             // Handle unauthorized user
             if (!isHost) throw new UnauthorizedException('Unauthorized user!');
 
             // If user is host -> Handle kick (guest leave)
             await this.lobbyLogicService.handleGuestLeave(gameId);
+
             // Prepare update event for the users in the hub, signalling that game is open again
             const lobbiesData = await this.hubLogicService.getOpenLobbies();
+
             return [
                 createLobbyToHubEvent(),
                 createLobbyGuestLeftEvent(),
@@ -137,11 +136,9 @@ export class GameLobbyService implements IGameLobbyService {
 
     async handleStartGameMessage(startData: StartGameDataType): Promise<LobbyEventMovedToGame | ErrorEvent> {
         try {
-            console.log('Game Lobby Service - Handle Start Game Message Fired');
-            const { gameId, playerId } = startData;
-            console.log(`GameId: ${gameId}, playerId: ${playerId}`);
+            const { gameId } = startData;
+
             const isHost = await this.lobbyLogicService.isPlayerHost(startData);
-            console.log('Handle start message, is host - ', isHost);
 
             // Handle unauthorized user
             if (!isHost) throw new UnauthorizedException('Unauthorized user!');
--- src/game/game_lobby/services/lobby_logic/lobby_logic.service.ts
+++ src/game/game_lobby/services/lobby_logic/lobby_logic.service.ts
@@ -22,11 +22,8 @@ export class LobbyLogicService implements ILobbyLogicService {
     ) { }
 
     async startGame(gameId: string): Promise<ActiveLobbyDataDTO> {
-        console.log('START GAME SERVICE FIRED'); console.log(gameId);
-
         const startedGame = await this.gameService.updateGameStatus({ gameId, newStatus: GameStatus.InProgress });
 
-        console.log('New active game:'); console.log(startedGame);
         // We started game -> narrow the status type
         const lobbyData = this.gameToLobbyData(startedGame) as ActiveLobbyDataDTO;
 
@@ -34,9 +31,9 @@ export class LobbyLogicService implements ILobbyLogicService {
     }
 
     async isValidLobbyConnection(authData: LobbyAuthDTO): Promise<LobbyDataDTO> {
-        console.log('VALIDATE LOBBY SERVICE FIRED'); console.log(authData);
         const authDTO = plainToClass(LobbyAuthDTO, authData);
         const errors = await validate(authDTO);
+
         if (errors.length > 0)
             throw new UnauthorizedException('Invalid lobby credentials!');
 
@@ -46,39 +43,30 @@ export class LobbyLogicService implements ILobbyLogicService {
     }
 
     async isPlayerHost(leaveLobbyData: LeaveLobbyDataType): Promise<boolean> {
-        console.log('LOBBY LOGIC - IS PLAYER HOST');
         const { gameId, playerId } = leaveLobbyData;
         const game = await this.gameService.getGameById({ gameId });
 
         if (!game)
             throw new NotFoundException('Game not found!');
-        console.log('IsPlayerHost - Game found'); console.log(game);
 
         const { host: { id: hostId }, guest } = game;
+
         if (playerId !== hostId && playerId !== guest?.id)
             throw new UnauthorizedException('Player is not a part of the game!');
-        console.log('IsPlayerHost - Player is part of the game. Is host - ', playerId === hostId);
+
         return playerId === hostId;
     }
 
     async handleGuestLeave(gameId: string): Promise<void> {
-        console.log('LOBBY LOGIC - HANDLE GUEST LEAVE');
-
         // Clear the guest from the game
-        const vacantGame = await this.gameService.kickGuest({ gameId });
-
-        console.log('Handle guest leave, updated game:'); console.log(vacantGame);
+        await this.gameService.kickGuest({ gameId });
 
         return;
     }
 
     async handleHostLeave(gameId: string): Promise<void> {
-        console.log('LOBBY LOGIC - HANDLE HOST LEAVE');
-
         // Change game status
-        const abortedGame = await this.gameService.updateGameStatus({ gameId, newStatus: GameStatus.Aborted });
-
-        console.log('Handle host leave, updated game:'); console.log(abortedGame);
+        await this.gameService.updateGameStatus({ gameId, newStatus: GameStatus.Aborted });
 
         return;
     }
@@ -87,26 +75,38 @@ export class LobbyLogicService implements ILobbyLogicService {
         const { id: gameId, host: { id: hostId, name: hostName }, guest, status } = game;
 
         return guest
-            ? { gameId, host: { hostId, hostName }, guest: { guestId: guest.id, guestName: guest.name }, status, turns: [] }
-            : { gameId, host: { hostId, hostName }, guest: null, status, turns: [] };
+            ? {
+                gameId,
+                host: { hostId, hostName },
+                guest: { guestId: guest.id, guestName: guest.name },
+                status,
+                turns: []
+            }
+            : {
+                gameId,
+                host: { hostId, hostName },
+                guest: null,
+                status,
+                turns: []
+            };
     }
 
     private async fetchPlayerGame(lobbyAuthData: LobbyAuthDTO): Promise<Game> {
         const { gameId, userId } = lobbyAuthData;
-        console.log('LOBBY GET GAME'); console.log(lobbyAuthData);
+
         const player = await this.playerService.getPlayerById({ id: userId });
-        console.log('PLAYER:'); console.log(player);
+
         if (!player)
             throw new NotFoundException('Player not found!');
 
         const game = await this.gameService.getGameById({ gameId });
-        console.log('GAME:'); console.log(game);
+
         if (!game)
             throw new NotFoundException('Game not found!');
 
         if (game.status !== GameStatus.Pending && game.status !== GameStatus.InProgress)
             throw new BadRequestException('Incorrect game status!');
-        console.log('Role check:', userId, game.host, game.guest);
+
         if (game.host.id !== userId && game.guest?.id !== userId)
             throw new UnauthorizedException('Unauthorized player!');
 
